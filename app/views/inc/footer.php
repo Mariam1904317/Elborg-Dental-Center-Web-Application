2b87eba7c3b1ab201d0b95fadbe562a0d5fab06f
Mariam1904317 <96684192+Mariam1904317@users.noreply.github.com>
2022-06-01 19:03:23 +0200

    Create footer.php

---
+++ app/views/inc/footer.php
@@ -0,0 +1,27 @@
+  </div>
+  <script src="<?php echo URLROOT; ?>css/jquery-3.5.1.min.js"></script>
+
+<script src="<?php echo URLROOT; ?>css/bootstrap.bundle.min.js"></script>
+
+
+<script src="<?php echo URLROOT; ?>css/wow.min.js"></script>
+
+<script src="<?php echo URLROOT; ?>css/theme.js"></script>
+
+<script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+
+
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<link rel="stylesheet" href="css/owl.carousel.js">
+
+<link rel="stylesheet" href="css/animate.css">
+
+<link rel="stylesheet" href="css/theme.css">
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  <script src="<?php echo URLROOT; ?>js/main.js"></script>
+  </body>
+
+  </html>
