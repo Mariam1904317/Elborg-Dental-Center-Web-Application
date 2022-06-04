6f3f2a1fefdff8ce62693f98cc447d31890342fb
Mariam1904317 <96684192+Mariam1904317@users.noreply.github.com>
2022-06-04 12:42:24 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -9,7 +9,7 @@ define('DB_NAME', 'miublog');
 //echo dirname(dirname(__FILE__));
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/mvc/public/');
+define('URLROOT', 'https://elborgdentalclinic.herokuapp.com/public/');
 // Site Name
 define('SITENAME', 'El Borg Dental Clinc');
 
